359134255f5e8b05250af69226eb0a76d94a0dfc
qmakar <qmakar@gmail.com>
2020-09-21 23:14:38 +0300

    HW_1

--- classify double/solution.c
+++ classify double/solution.c
@@ -2,8 +2,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-
-
+#include <math.h>
 
 /**
  * Library-level functions.
@@ -14,98 +13,120 @@ uint64_t convertToUint64 (double number) {
     return *((uint64_t *)(&number));
 }
 
-bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
-}
-
 
 /**
  * Checkers here:
  */
 
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == 0;
+}
+
+bool checkForPlusDenormal (uint64_t number) {
+    return number > 0 && \
+    number < 0x0010000000000000;
+}
+
+bool checkForPlusNormal (uint64_t number) {
+    return number >= 0x0010000000000000 && \
+    number < 0x7FF0000000000000;
+}
+
+bool checkForPlusInf (uint64_t number) {
+    return number == 0x7FF0000000000000;
+}
+
+bool checkForSignalingNan (uint64_t number) {
+    return number > 0x7FF0000000000000 && \
+    number < 0x7FF8000000000000 ||
+    number > 0xFFF0000000000000 && \
+    number < 0xFFF8000000000000
+    ;
+}
+
+bool checkForQuietNan (uint64_t number) {
+    return number >= 0x7FF8000000000000 && \
+    number < 0x8000000000000000 ||
+    number >= 0xFFF8000000000000 && \
+    number <= 0xFFFFFFFFFFFFFFFF
+    ;
 }
 
 bool checkForMinusZero (uint64_t number) {
     return number == 0x8000000000000000;
 }
 
-bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
-}
-
-bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
-}
-
-bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusDenormal (uint64_t number) {
+    return number > 0x8000000000000000 && \
+    number < 0x8010000000000000;
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    return number >= 0x8010000000000000 && \
+    number < 0xFFF0000000000000;
 }
 
-bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusInf (uint64_t number) {
+    return number == 0xFFF0000000000000;
 }
 
-bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
-}
 
-bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
-}
-
-bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
-}
 
 
 void classify (double number) {
     if (checkForPlusZero(convertToUint64(number))) {
-        printf("Plus zero\n");
+        printf("Plus zero\n"); //0
     }
 
     else if (checkForMinusZero(convertToUint64(number))) {
-        printf("Minus zero\n");
+        printf("Minus zero\n"); //-0
     }
 
     else if (checkForPlusInf(convertToUint64(number))) {
-        printf("Plus inf\n");
+        printf("Plus inf\n"); //1.0 / 0;
     }
 
     else if (checkForMinusInf(convertToUint64(number))) {
-        printf("Minus inf\n");
+        printf("Minus inf\n"); //-1.0 / 0;
     }
 
     else if (checkForPlusNormal(convertToUint64(number))) {
-        printf("Plus normal\n");
+        printf("Plus normal\n"); // 1     .. 1.7976931348623157E+308
     }
 
     else if (checkForMinusNormal(convertToUint64(number))) {
-        printf("Minus normal\n");
+        printf("Minus normal\n"); // -1
     }
 
     else if (checkForPlusDenormal(convertToUint64(number))) {
-        printf("Plus Denormal\n");
+        printf("Plus Denormal\n"); // 4.940656e-324 .. 2.225074e-308
     }
 
     else if (checkForMinusDenormal(convertToUint64(number))) {
-        printf("Minus Denormal\n");
+        printf("Minus Denormal\n"); // -4.940656e-324 .. -2.225074e-308
     }
 
     else if (checkForSignalingNan(convertToUint64(number))) {
-        printf("Signailing NaN\n");
+        printf("Signailing NaN\n"); // std::numeric_limits<double>::signaling_NaN();
     }
 
     else if (checkForQuietNan(convertToUint64(number))) {
-        printf("Quiet NaN\n");
+        printf("Quiet NaN\n"); // 0.0 / 0     sqrt(-2) INF-INF   INF/INF   0*INF
     }
 
     else {
         printf("Error.\n");
     }
-}
+} 
+
+
+
+//int main(){
+//    uint64_t r = 0x7FF000000000000F;
+//
+//    double a = *((double *)(&r));
+//    double b;// = 4.940656e-324;
+//    printf("%e\n", a);
+//    classify(b);
+//}
+
