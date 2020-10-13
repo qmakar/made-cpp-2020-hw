6b24e09eaee0ca79a9e82c0d303d7a43ea7bb44e
qmakar <qmakar@gmail.com>
2020-10-13 23:28:22 +0300

    hw_4

--- matrix/src/matrix.h
+++ matrix/src/matrix.h
@@ -1,3 +1,10 @@
+//
+//  matrix.hpp
+//  matrix_xcode
+//
+//  Created by Makar Q on 13.10.2020.
+//
+
 #pragma once
 
 #include <vector>
@@ -9,26 +16,42 @@ namespace task {
 const double EPS = 1e-6;
 
 
-class OutOfBoundsException : public std::exception {};
-class SizeMismatchException : public std::exception {};
+class OutOfBoundsException : public std::exception {
+public:
+    const char* what() const noexcept override {
+            return "OutOfBoundsException";
+        }
+};
 
+class SizeMismatchException : public std::exception {
+public:
+    const char* what() const noexcept override {
+            return "SizeMismatchException";
+        }
+};
+class MatrixProxy;
 
 class Matrix {
-
+    size_t rows_, cols_;
+    double** matrix_;
+    
+    friend class MatrixProxy;
 public:
 
     Matrix();
     Matrix(size_t rows, size_t cols);
     Matrix(const Matrix& copy);
     Matrix& operator=(const Matrix& a);
+    Matrix(const Matrix& copy, size_t col);
+    ~Matrix();
 
     double& get(size_t row, size_t col);
     const double& get(size_t row, size_t col) const;
     void set(size_t row, size_t col, const double& value);
     void resize(size_t new_rows, size_t new_cols);
 
-    /* ??? */ operator[](size_t row);
-    /* ??? */ operator[](size_t row) const;
+    MatrixProxy operator[](size_t row);
+    const MatrixProxy operator[](size_t row) const;
 
     Matrix& operator+=(const Matrix& a);
     Matrix& operator-=(const Matrix& a);
@@ -53,9 +76,23 @@ public:
 
     bool operator==(const Matrix& a) const;
     bool operator!=(const Matrix& a) const;
+    
+    size_t getRows() const;
+    size_t getCols() const;
 
-    // Your code goes here...
+};
 
+class MatrixProxy{
+    size_t cols_;
+    double* row_;
+    
+public:
+    MatrixProxy(Matrix* m, size_t row_idx);
+    MatrixProxy(const Matrix* m, size_t row_idx);
+    
+    double& operator [] (size_t col_idx);
+    const double operator [] (size_t col_idx) const;
+    
 };
 
 
